ff051c27691df804d63b2aee02f83ff65eda3684
cxy <347025712@qq.com>
2017-11-10 20:02:22 +0800

    rasdfa

--- index.js
+++ index.js
@@ -2,122 +2,42 @@ import React,{Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+const lessons=[
+    {title:'a',des:'aaa'},
+    {title:'b',des:'bbb'},
+    {title:'c',des:'ccc'}
+]
 
-//class LikeButton extends Component
-//{
-//    static defaultProps={
-//        likeText:'取消',
-//        unlikeText:'点赞'
-//    }
-//
-//    constructor()
-//    {
-//        super()
-//        this.state={isLike:false}
-//    }
-//
-//    handleClick()
-//    {
-//        this.setState(
-//            {
-//
-//                isLike:!this.state.isLike
-//            }
-//        )
-//        console.log(this.props.likeText)
-//    }
-//    render()
-//    {
-//        //const likeText=this.props.likeText||'点赞'
-//        //const unlikeText=this.props.unlikeText||'取消'
-//        return(
-//            <button onClick={this.handleClick.bind(this)}>
-//                {this.state.isLike?this.props.likeText:this.props.unlikeText}
-//            </button>
-//        )
-//    }
-//}
-//
-//class Index extends Component
-//{
-//    constructor()
-//    {
-//        super()
-//        this.state={
-//            likeText:'已赞' ,
-//            unlikeText:'赞'
-//        }
-//    }
-//
-//    handleClick()
-//    {
-//        this.setState(
-//            {
-//                likeText:'取消',
-//                unlikeText:'点赞'
-//            }
-//        )
-//    }
-//    render()
-//    {
-//        return(
-//            <div>
-//                <LikeButton likeText={this.state.likeText} unlikeText={this.state.unlikeText} />
-//
-//                <div>
-//                    <button onClick={this.handleClick.bind(this)}>
-//                        修改
-//                    </button>
-//                </div>
-//            </div>
-//        )
-//    }
-//}
-
-
-
-class Screen extends Component
+class Lesson extends Component
 {
-    static defaultProps={showContent:'无内容'}
     render()
     {
-       return(
-           <div>
-               {this.props.showContent}
-           </div>
-       )
-    }
-}
-class Computer extends Component
-{
-    constructor()
-    {
-        super()
-        this.state={status:'off'}
-    }
-    //OnclickEvent()
-    //{
-    //    this.setState(
-    //        {
-    //            OnOff:!this.state.OnOff
-    //        }
-    //    )
-    //}
-
-    render()
-    {
+        const {lesson}=this.props
         return(
             <div>
-                <button onClick={()=>this.setState({status:this.state.status=='off'?'on':'off'})}>
-                    开关
-                </button>
-                <Screen showContent={this.state.status == 'off'?'显示屏关了':'显示屏亮了'}/>
+                <h1>{lesson.title}</h1>
+                <p>{lesson.des}</p>
             </div>
         )
     }
 }
+
+class LessonList extends Component
+{
+    render()
+    {
+        return(
+                <div>
+                    {lessons.map((lesson)=><Lesson  lesson={lesson} />)}
+                </div>
+            )
+
+
+
+    }
+}
+
 ReactDOM.render(
-    <Computer />,
+    <LessonList />,
     document.getElementById('root')
 )
-
